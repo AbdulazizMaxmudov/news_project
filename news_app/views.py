c470a630df475bfafc325effe5b41fda9fb28952
AbdulazizMaxmudov <alazizmaxmudov@gmail.com>
2025-11-13 18:44:03 +0500

    first django project started

---
+++ news_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render , get_object_or_404
+from .models import News , Category
+
+
+def news_list(request):
+    news_list = News.published.all()
+    context = {
+        'news_list': news_list
+    }
+
+    return render(request, 'news/news_list.html', context = context)
+
+
+
+def news_detail(request,id):
+    news = get_object_or_404(News , id=id  , status=News.Status.Published )
+    context = {
+        'news': news
+    }
+    return render(request, 'news/news_detail.html', context = context)
+# Create your views here.
